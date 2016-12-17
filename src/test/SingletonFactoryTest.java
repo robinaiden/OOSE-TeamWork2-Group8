7236406178f499443896b9f802f52e0117f0d889
robinaiden <a10423041@yuntech.org.tw>
2016-12-17 14:21:37 +0800

    Add: JUnit4單元測試

--- src/test/SingletonFactoryTest.java
+++ src/test/SingletonFactoryTest.java
@@ -1,23 +1,33 @@
 package test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import singleton_template_factory.BlackTeaFactory;
+import singleton_template_factory.GreenTeaFactory;
+import singleton_template_factory.TeaFactory;
 
 public class SingletonFactoryTest {
 
-//	@Test
-//	public void createLatteTest() {
-//		TeaFactory coffeeFactory = new TeaFactory();
-//		TeaProduct blackteaTest = coffeeFactory.createBlackTea();
-//		assertNotNull(blackteaTest);
-//	}
-//	
-//	@Test
-//	public void createMochaTest() {
-//		TeaFactory coffeeFactory = new TeaFactory();
-//		TeaProduct greenteaTest = coffeeFactory.createGreenTea();
-//		assertNotNull(greenteaTest);
-//	}
+	@Test
+	public void BlackTeaFactoryTest() {
+		TeaFactory teafactory1 = BlackTeaFactory.getInstance();
+		TeaFactory teafactory2 = BlackTeaFactory.getInstance();
+		assertSame(teafactory1, teafactory2);
+	}
+	
+	@Test
+	public void GreenTeaFactoryTest() {
+		TeaFactory teafactory1 = GreenTeaFactory.getInstance();
+		TeaFactory teafactory2 = GreenTeaFactory.getInstance();
+		assertSame(teafactory1, teafactory2);
+	}
+	
+	@Test
+	public void LemonTeaFactoryTest() {
+		TeaFactory teafactory1 = GreenTeaFactory.getInstance();
+		TeaFactory teafactory2 = GreenTeaFactory.getInstance();
+		assertSame(teafactory1, teafactory2);
+	}
 
 }
